1ad705f450d2397553172833926e59227e0b64f6
Rayan <rayan.oughlis@etu.u-pec.fr>
2026-09-03 11:35:38 +0200

    chore: set up ESLint, Prettier, Husky, lint-staged and commitlint
    Flat ESLint config with typescript-eslint and the React Hooks rules, Prettier
    with a shared style, and Git hooks: pre-commit runs lint-staged, commit-msg
    runs commitlint (Conventional Commits enforced).

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'refactor', 'perf', 'docs', 'style', 'test', 'build', 'ci', 'chore', 'revert'],
+    ],
+    'subject-case': [0],
+  },
+};
